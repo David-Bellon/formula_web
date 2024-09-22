2b744190c5af29307c862c83cb4e3b92708e42b5
David-Bellon <91338053+David-Bellon@users.noreply.github.com>
2024-09-23 00:47:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 import streamlit as st
+import pandas as pd
+import fastf1
+import plotly.graph_objects as go
 
 st.title("My First Streamlit Webpage")
 
-# Add a header
-st.header("Welcome to My Streamlit App")
+session = fastf1.get_session(2024, 15, 'Q')
+session.load()
 
-# Add a subheader
-st.subheader("This is a simple interactive web application")
+ver_lap = session.laps.pick_driver('VER').pick_fastest()
+ham_lap = session.laps.pick_driver('HAM').pick_fastest()
 
-# Add some text
-st.write("Streamlit is an open-source Python library that makes it easy to create and share beautiful, custom web apps for machine learning and data science.")
+ver_tel = ver_lap.get_car_data().add_distance()
+ham_tel = ham_lap.get_car_data().add_distance()
 
-# Add a slider
-age = st.slider("Select your age:", 0, 100, 25)
-st.write(f"Your age is {age}.")
+ver_data = ver_tel[["Distance", "Speed"]]
+ham_data = ham_tel[["Distance", "Speed"]]
+df = pd.DataFrame(columns=["Distance", "Speed_1", "Speed_2"])
+df["Distance"] = ver_data["Distance"]
+df["Speed_1"] = ver_data["Speed"]
+df["Speed_2"] = ham_data["Speed"]
 
-# Add a button
-if st.button("Click me!"):
-    st.write("Button clicked!")
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(
+    x=ham_data["Distance"],
+    y=ham_data["Speed"],
+    mode="lines",
+    name="HAM",
+    line=dict(color="red")
+))
+
+fig.add_trace(go.Scatter(
+    x=ver_data["Distance"],
+    y=ver_data["Speed"],
+    mode="lines",
+    name="VER",
+    line=dict(color="blue")
+))
+
+fig.update_layout(title="Comparation", xaxis_title="Distance", yaxis_title="Speed")
+
+st.plotly_chart(fig)
